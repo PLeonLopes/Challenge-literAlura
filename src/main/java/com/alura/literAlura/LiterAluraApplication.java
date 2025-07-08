9e6adb877e93054164ea51f8a4f10448eab63583
Pedro Lopes <109923335+PLeonLopes@users.noreply.github.com>
2025-07-08 16:10:35 -0300

    feat: Finalização do projeto

--- src/main/java/com/alura/literAlura/LiterAluraApplication.java
+++ src/main/java/com/alura/literAlura/LiterAluraApplication.java
@@ -2,12 +2,25 @@ package com.alura.literAlura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.alura.literAlura.principal.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class LiterAluraApplication {
+public class LiterAluraApplication implements CommandLineRunner {
+	@Autowired
+	private Principal principal;
+
+	public LiterAluraApplication(Principal principal) {
+		this.principal = principal;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		principal.exec();
+	}
 }
